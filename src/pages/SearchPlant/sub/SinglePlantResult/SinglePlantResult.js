4238d709ed4f9e0e8ef645d11ef867063ed340d1
SebastianKostrz <sebastian.kostrz02@gmail.com>
2023-11-06 14:43:10 +0100

    add plant page component with subcomponents and styling

--- src/pages/SearchPlant/sub/SinglePlantResult/SinglePlantResult.js
+++ src/pages/SearchPlant/sub/SinglePlantResult/SinglePlantResult.js
@@ -1,9 +1,14 @@
 import './SinglePlantResult.css'
+import { useNavigate } from 'react-router-dom';
 
-export default function SinglePlantResult({plantImageUrl,plantName}){
+export default function SinglePlantResult({plantImageUrl,plantName,id}){
+    const navigate = useNavigate();
+    const navigateToPlant=()=>{
+        navigate("/plant/"+id);
+    }
     return (
         <>
-            <div className='single-plant'>
+            <div className='single-plant' onClick={navigateToPlant}>
                 <div className='plant-image'>
 
                 </div>
