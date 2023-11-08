5028eaadbacb4664c3fcd3e68fb2ab29c827e276
Pawel Ignaczak <ignak265@gmail.com>
2023-11-08 14:46:53 +0100

    Feature/ protected routes
    Feature: Home page for logged in user and forum are now protected.
    Rework: removed profile page, profile will be managed by my account page
    Merged with feature/my-account-page branch

--- src/utils/PrivateRoutes.js
+++ src/utils/PrivateRoutes.js
@@ -1,15 +1,12 @@
-import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-
-//contexts
-import { isAuthenticatedContext } from "../App";
 
 //constants
 import PAGES from "../constants/pages";
 
-export default function PrivateRoutes(){
-    const isAuthenticated = useContext(isAuthenticatedContext);
+export default function PrivateRoutes(props){
+    const {isAuthenticated} = props;
     let auth = {"token": isAuthenticated}
+
     return(
         auth.token ? <Outlet/> : <Navigate to={PAGES.LOGIN}/>
     )
