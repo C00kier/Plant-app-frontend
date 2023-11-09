eb6c415a76edc41abf2c539560696a0083e788ba
Pawel Ignaczak <ignak265@gmail.com>
2023-11-09 10:46:58 +0100

    Fix/ fixed main container overlaping footer
    fixed main container overlaping footer
    rework: moved styles for app children from index.css to app.css
    feature: added flex-column and flex-row common classes

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
               <nav>
                 <Navbar />
               </nav>
-              <main>
+              <main className='flex-column-center-center'>
                 <Outlet/>
               </main>
               <footer>
