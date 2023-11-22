fa8326cf9f17e12d6b77488d85c1e549f9dea27a
SebastianKostrz <sebastian.kostrz02@gmail.com>
2023-11-22 14:47:20 +0100

    Fix: fix buttons not working properly

--- src/components/Quiz/sub/QuizFooter/QuizFooter.js
+++ src/components/Quiz/sub/QuizFooter/QuizFooter.js
@@ -14,20 +14,20 @@ export default function QuizFooter({ manageIndex, index, submit, isCurrentQuesti
         <>
             <div id='quiz-footer'>
                 <div id='prev-button' className={index == 1 ? 'quiz-nav-button-inactive' : 'quiz-nav-button'} onClick={(e) => manageIndex(e)}>
-                    <span id='back' className='quiz-button-writing'>Wstecz</span>
+                    <span id='back' className='quiz-button-writing' onClick={(e) => manageIndex(e)}>Wstecz</span>
                 </div>
                 {index != 6 ?
                     (isCurrentQuestionAnswered ?
                         <div id='next-button' className={index == 6 || !isNextButtonActive ? 'quiz-nav-button-inactive' : 'quiz-nav-button'} onClick={(e) => manageIndex(e)}>
-                            <span id='next' className='quiz-button-writing'>Dalej</span>
+                            <span id='next' className='quiz-button-writing' onClick={(e) => manageIndex(e)}>Dalej</span>
                         </div>
                         :
                         <div id='next-button' className='quiz-nav-button-inactive'>
-                            <span id='next' className='quiz-button-writing'>Dalej</span>
+                            <span id='next' className='quiz-button-writing' onClick={(e) => manageIndex(e)}>Dalej</span>
                         </div>
                     )
                     : (isCurrentQuestionAnswered ? <div id='submit-button' onClick={submit} className='quiz-nav-button'>
-                        <span id='submit' className='quiz-button-writing'>Wyślij!</span>
+                        <span id='submit' className='quiz-button-writing' >Wyślij!</span>
                     </div> :
                         <div id='next-button' className='quiz-nav-button-inactive'><span id='submit' className='quiz-button-writing'>Wyślij!</span></div>
                     )}
