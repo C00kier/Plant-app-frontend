58cf608f2bdf112189f3690349f0b39bedb82d14
SebastianKostrz <sebastian.kostrz02@gmail.com>
2023-11-23 13:45:33 +0100

    Feature: Add plant adding feature, Loading screen feature

---
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,13 @@
+import './LoadingScreen.css';
+
+export default function LoadingScreen(){
+    return (<>
+        <div id='loading-screen-background-shade'>
+            <div id='loading-screen'>
+                <div id='loading-screen-gif'>
+                    
+                </div>
+            </div>
+        </div>
+    </>)
+}
