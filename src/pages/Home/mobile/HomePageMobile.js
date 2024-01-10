a0c24bca1c9cd1a89303b85b0f1172e0f3a2f5fe
izabonarowska <ibonarowska@gmail.com>
2024-01-10 11:11:42 +0100

    Fix: Home Page Mobile

--- src/pages/Home/mobile/HomePageMobile.js
+++ src/pages/Home/mobile/HomePageMobile.js
@@ -15,22 +15,18 @@ export default function HomePageMobile(){
                     <p>Odkryj nową aplikację, która uczyni twoje życie roślinnym, łatwiejszym i pełniejszym!</p>
                     <Link to={PAGES.REGISTER}><button className="home-page-mobile-register-button" type="button">Zarejestruj się!</button></Link>
                 </div>
-                <div className="home-page-mobile-upper-right-container">
-                    <img className="home-page-mobile-image" src={blankImage} alt="plant"/>
-                </div>
             </div>
             <div className="home-page-mobile-lower-container flex-column-center-center">
                 <div className="horizontal-thin-grey-line"/>
                 <div className="home-page-mobile-lower-text-box flex-column-center-center">
-                    <p className="home-page-mobile-lower-text-box-header">Odkryj z nami świat roślin!</p>
                     <p className="home-page-mobile-lower-text-box-description">Dostarczymy spersonalizowane rekomendacje dotyczące najlepszych gatunków roślin do Twojego domu i przypomnimy Ci o ich podlewaniu i nawożeniu.</p>
                 </div>
                 <iframe
                     className="home-page-mobile-video"
                     width="300"
                     height="240"
-                    src="https://www.youtube.com/embed/plwwJsWiO6U"
-                    title="Canterbury Park Corgi Races August 1, 2021"
+                    src="https://www.youtube.com/embed/RRMcCNbmFWc?si=qhdE-9NPtB4HHlBi"
+                    title="Sprout"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;"
                     allowFullScreen>
                 </iframe>
