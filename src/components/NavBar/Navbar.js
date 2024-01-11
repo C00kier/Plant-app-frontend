df17433700c215261897a3e54434bc39233c38e9
izabonarowska <ibonarowska@gmail.com>
2024-01-11 14:50:46 +0100

    Fix: Navbar Mobile CSS

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,15 +1,25 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import "./Navbar.css";
 import PAGES from '../../constants/pages';
 import logo from '../../../src/assets/logo/sprout_logo.png'
 import userIconImage from '../../../src/assets/user/user-circle.256x256.png';
+import hamburger from '../../assets/accountSidebar/hamburger_icon.png'
+
+//constants
+import COMPONENT_STATE from "../../constants/myAccountComponentStates.js";
+
+//contexts
+import { functionalityElementContext } from "../../pages/Home/loggedUser/HomePageLogged.js";
+
 
 export default function Navbar(props) {
     const { cookies, removeCookie } = props;
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [myAccountDropdownOpen, setMyAccountDropDownOpen] = useState(false);
     const navigate = useNavigate();
-    
+    const setFunctionalityElement = useContext(functionalityElementContext);
+
     const handleSignOut = () => {
         removeCookie("token");
         removeCookie("userId");
@@ -20,10 +30,14 @@ export default function Navbar(props) {
         setDropdownOpen(!dropdownOpen);
     };
 
+    const toggleMyAccountDropdown = () => {
+        setMyAccountDropDownOpen(!myAccountDropdownOpen);
+    }
+
     const commonLinks = (
         <div className="navbar-links-container">
             <ul>
-            {cookies.token ? (<li></li>) : (<li className='navbar-dropdown-links-bold'><Link to={PAGES.LOGIN}>Zaloguj się</Link></li>)}
+                {cookies.token ? (<li></li>) : (<li className='navbar-dropdown-links-bold'><Link to={PAGES.LOGIN}>Zaloguj się</Link></li>)}
                 <li><Link to={PAGES.SEARCH}>Szukaj roślin</Link></li>
                 <li><Link to={PAGES.BLOG}>Blog</Link></li>
             </ul>
@@ -49,6 +63,20 @@ export default function Navbar(props) {
         </div>
     );
 
+    const myAccountIcon = (
+        <div className="my-account-icon" onClick={toggleMyAccountDropdown}>
+            <img className="my-account-icon-image" src={hamburger} alt="hamburger-icon" />
+            {myAccountDropdownOpen && (
+                <div className="navbar-dropdown-container">
+                    <ul className="myAccount-dropdown-links">
+                        <li onClick={() => setFunctionalityElement(COMPONENT_STATE.COCKPIT)}>Kokpit</li>
+                        <li onClick={() => setFunctionalityElement(COMPONENT_STATE.RECOMMENDATION)}>Rekomendacje</li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+
     return (
         <div className='navbar-div'>
             <div className="navbar-logo">
@@ -57,7 +85,10 @@ export default function Navbar(props) {
             {cookies.token ? (
                 <>
                     {commonLinks}
-                    {userIcon}
+                    <div className='navbar-icons'>
+                        {userIcon}
+                        {myAccountIcon}
+                    </div>
                 </>
             ) : (
                 <>
@@ -68,7 +99,7 @@ export default function Navbar(props) {
                             <div className="navbar-dropdown-container">
                                 <ul className="navbar-dropdown-links">
                                     <li className="navbar-dropdown-links-bold">
-                                    <Link to={PAGES.REGISTER}>Zarejestruj się</Link></li>
+                                        <Link to={PAGES.REGISTER}>Zarejestruj się</Link></li>
                                     <li><Link to={PAGES.LOGIN}>Zaloguj się</Link></li>
                                     <li><Link to={PAGES.BLOG}>Blog</Link></li>
                                     <li><Link to={PAGES.SEARCH}>Szukaj roślin</Link></li>
